9ff6232349924a4b3b8880c9f8b24b99658a4212
Bhawish Kumar <23205172@life.hkbu.edu.hk>
2026-03-02 22:59:36 +0800

    Add self-introduction feature

--- src/main/kotlin/bhawishkumar/SelfIntroduction.kt
+++ src/main/kotlin/bhawishkumar/SelfIntroduction.kt
@@ -1,8 +1,12 @@
 package hk.edu.hkbu.comp.comp4107.playground.bhawishkumar
 
-fun selfIntroduction() {
-    println("Hello, my name is Bhawish Kumar.")
-    println("I am a student at HKBU.")
-    println("I am studying Computer Science.")
-    println("I am interested in machine learning.")
+class SelfIntroduction{
+
+    fun selfIntroduction() {
+        println("Hello, my name is Bhawish Kumar.")
+        println("I am a student at HKBU.")
+        println("I am studying Computer Science.")
+        println("I am interested in machine learning.")
+    }
+
 }
